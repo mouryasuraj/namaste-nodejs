8373196dff6323e0cabed72a7e225f02beadbdcf
Suraj Mourya <mouryasuraj6266444@gmail.com>
2024-08-12 23:53:27 +0530

    02.IntroductionToNodeJS Completed

---
+++ 02.IntroductionToNodeJS/notes.js
@@ -0,0 +1,25 @@
+// Introduction to NodeJS
+
+/*
+
+1. Run JavaScipt Everywhere.
+2. NodeJS is a JavaScript Runtime built on Chrome's V8 JavaScipt Engine.
+3. NodeJs is a cross platform(It can be run on any device or Operating System).
+4. It is an open source maintain by OpenJS Foundation.
+5. NodeJS has an event-driven architecture capable of asynchronous I/O(Non-blocking I/O).
+6. It's first version released in 2009 and was developed by Ryan Dahl.
+
+// History of NodeJS
+
+1. In 2009 NodeJS was born and created by Ryan Dahl.
+2. Whatever we write in NodeJS is a javascript code and we cannot run javascript code without JS Engine.
+3. Wherever there is a javascipt, there will a javascript engine.
+4. Every browser runs javascript and every browser have their own javascript engine. 
+5. Chrome(V8 Engine), Firfor(Spider Monkey), Safari(JavascriptCore)
+6. Joyent is a software company which used nodejs and build some projects using it in initial stage of nodejs.
+7. First name of nodejs was webjs.
+8. In 2010, npm was build. NPM is a package manager for NodeJS. It is resitry where all the packages are build and stored.
+9. Initially, nodejs was build for only mac os and linux. Later in 2011, it was also build for windows and it was led by Joyent and Mircosoft.
+10. In 2012, Ryan left the nodejs project and then it was handled by Isaac Z who created npm.
+
+*/
