e56fc84d3f3b622bf76d5b5091dc69c83582dcde
Suraj Mourya <mouryasuraj6266444@gmail.com>
2024-09-04 23:56:21 +0530

    completed 'How nodejs works behind the scenes' still pending ep5

---
+++ 06.DivingDeepIntoNodejsGithubRepo/app.js
@@ -0,0 +1,10 @@
+require('./other.js')
+
+/*
+function () {
+    
+// Javascript create a function and wrapp all the codes present inside other.js and then run it. because of that we cannot access those variables and functions
+
+}
+
+*/
--- 06.DivingDeepIntoNodejsGithubRepo/notes.js
+++ 06.DivingDeepIntoNodejsGithubRepo/notes.js
@@ -1,3 +1,39 @@
 // Diving deep into nodejs github 
 
 
+/*
+
+1. Whenever we create a function or wrap some piece of code then it becomes private, we cannot access those variabls and functions which are present over there.
+2. Modules work the same way javascript
+3. All the codes which are present inside a module are wrapped into a function and then it executes. so we cannot access those variables and function.
+4. The only way to access those variables and functions by module.exports(Common JS module) or export(ES Module)
+5. require("./path") --> ALl the code inside the require are wrapped into a function and this function is a special function called IIFE(Immediately Invoked Function Expression)
+    ex:     (function(){
+                //ALl the code of a module wrapped and put it here
+            })()
+6. Before giving to V8 Engine. It wraps all code inside IIFE
+
+*/
+
+// QUestions
+
+/*
+
+1. How are variables and function private in different modules?
+--> Beacuse all the code are wrapped inside IIFE functions
+
+3. How do you get access to module.exports?
+--> (function (module, require)=>{
+        require("./path")
+        const a = 10;
+        console.log(a);
+
+        module.exports = {a}
+    })(module.exports = {}, require);
+    2. Node js is providing module.exports
+    3. Nodejs passes module and require as a parameter to the IIFE
+    4. When these codes are wrapped inside a function then only it pass to V8 Engine
+
+
+    //Resume from : 1:04: 00 from 5 steps mechanism of require
+*/
