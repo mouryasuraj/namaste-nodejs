536363d77e88e0d93b5e8bd2dfadb6d06f789f05
Suraj Mourya <mouryasuraj6266444@gmail.com>
2025-04-03 23:26:00 +0530

    Completed S2E4 - Routing and Request handlers

---
+++ Season 2/04.RoutingandRequestHandlers/notes.js
@@ -0,0 +1,31 @@
+/*
+
+// Routing and Request Handlers
+
+
+1. Order matters in routes
+-->
+    ex: 
+        app.use("/home", (req,res)=>{
+            res.send("Hello")    
+        })
+        app.use("/", (req,res)=>{
+            res.send("/")    
+        })
+        app.use("/test/hello", (req,res)=>{
+            res.send("test hello")    
+        })
+
+2. What is HTTPS status code?
+--> 
+    1xx - informational response
+    2xx - Success
+    3xx - redirects
+    4xx - Client errors
+    5xx - Server errors
+
+3. app.use math all the http method api
+4. app.get means if we use http methods then it will only match that url and method also
+5. Explore more about routing and also about ?, +, * ,() and use of regex in routes
+6. Reading the query params and dynamic routes
+*/
--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -7,10 +7,67 @@ const app = express()  // Creating an express application or web server
 //     res.send("Hello from server") //It will send the response on any url
 // })
 
-app.use("/test",(req, res)=>{
-    res.send("Hello from server url") //It will send the response on a specified url
-})
+// order of the routes matters
 
+// app.use("/test",(req, res)=>{
+//     res.send("Hello from server url") //It will send the response on a specified url
+// })
+// app.use("/hello",(req, res)=>{
+//     res.send("Hello from server hello hello") //It will send the response on a specified url
+// })
+
+// app.use("/",(req, res)=>{
+//     res.send("Hello from server") //It will send the response on any url
+// })
+
+
+
+//http methods (get, post, put, patch, delete)
+
+
+// This will only handle get call to /user
+// app.get("/user", (req,res)=>{
+//     res.send({firstName:"Suraj", lastName:"Mourya"})
+// })
+
+
+// // This will only handle post call to /user
+// app.post("/user", (req,res)=>{
+//     //Logic to save data to the database and then send response
+//     res.send("data saved successfully")
+// })
+
+
+// app.delete("/user", (req,res)=>{
+//     //Logic to delete data from the database and then send response
+//     res.send("data deleted successfully")
+// })
+
+
+
+
+// more about routes
+
+// app.get("/abc", (req,res)=>{
+//     res.send("Hello")
+// })
+
+
+// we can add any character between ab and cd ex: abSurajcd
+// app.get("/ab*cd", (req,res)=>{
+//     res.send("Hello1")
+// })
+
+// we can also use regex in url
+// If url inclued ab then the below endpoint will execute
+// app.get(/ab/,(req,res)=>{
+//     console.log(req.query);
+//     res.send(`Hello ${req.query.userid} = ${req.query.password}`)
+// })
+
+app.get("/user/:userId",(req,res)=>{
+    res.send(`Hello 123`)
+})
 
 
 
