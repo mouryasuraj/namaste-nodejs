fb4bcb362597ca4af0f039aea4fda65c06a99f21
Suraj Mourya <mouryasuraj6266444@gmail.com>
2025-07-28 22:14:21 +0530

    learned how to configure environment variable

---
+++ Season 3/05.Keeping Our Credentials Safe Using dotenv Files/note.js
@@ -0,0 +1,19 @@
+/*
+
+
+
+// Keeping Our Credentials Safe Using dotenv Files
+
+1. Don't push your secreat key and any confidential data in github or gitlab
+2. Instead use environment variables, you can use dontenv npm package to provide environment variables across the project
+3. npm i dotenv
+
+
+
+
+
+
+
+
+
+*/
