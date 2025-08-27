17684c7097bb869b1d21eff412f7efd3215e4e31
Suraj Mourya <mouryasuraj6266444@gmail.com>
2025-08-28 01:35:11 +0530

    added notes for razor pay

--- Season 3/07. Payment Gateway Integration ft. Razorpay/notes.js
+++ Season 3/07. Payment Gateway Integration ft. Razorpay/notes.js
@@ -27,6 +27,29 @@
 
 
 
+  Note:
+    1. We have to add this script in index.html file to include razor pay ui in the application
+    2. In React, use const rzp = window.Razorpay(options)
+                    rzp.open()
+    3. We can pass this option:
+        const options = {
+        key: 'YOUR_KEY_ID', // Replace with your Razorpay key_id
+        amount: '50000', // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+        currency: 'INR',
+        name: 'Acme Corp',
+        description: 'Test Transaction',
+        order_id: 'order_IluGWxBm9U8zJ8', // This is the order_id created in the backend
+        callback_url: 'http://localhost:3000/payment-success', // Your success URL
+        prefill: {
+          name: 'Gaurav Kumar',
+          email: 'gaurav.kumar@example.com',
+          contact: '9999999999'
+        },
+        theme: {
+          color: '#F37254'
+        },
+      };
+    <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
 
 
 */
